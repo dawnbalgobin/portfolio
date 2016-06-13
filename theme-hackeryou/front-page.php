ca60d978bbffea978264f7d70e07e7648b0c3a50
Dawn Balgobin <dawn.balgobin@gmail.com>
2016-06-12 21:40:05 -0400

    final

--- theme-hackeryou/front-page.php
+++ theme-hackeryou/front-page.php
@@ -64,6 +64,7 @@
   </div> <!-- skills -->
 
 <!-- PORTFOLIO -->
+<div class='port-desktop'>
  <div id="sectionthree"></div> 
  <div class="portfolio clearfix">
     <div class='wrapper'>
@@ -107,7 +108,7 @@
              <div class='port-text'>
               <h1> 3. </h1>
               <h2> Client API Project </h2>
-              <p>HTML5 / CSS3 / JavaScript / jQuery / API / Pair Programming / Git</p>
+              <p>HTML5 / CSS3 / JavaScript / jQuery / API / Git</p>
               <button class='viewit'><a href="#" target='_blank'>View</a></button>
              </div>
           </section>  
@@ -129,7 +130,78 @@
                </div>
           </section>   
         </div> 
+    </div>    
   </div> <!-- /portfolio -->
+
+  <!-- port-mobile -->
+  <div class='port-mobile'>
+  <div id="sectionthree"></div> 
+  <div class="portfolio clearfix">
+     <div class='wrapper'>
+         <div class='port-title'> 
+           <h3>Portfolio</h3>
+           <h4>Wanna see some stuff I created?</h4>
+           <div class='line'></div>
+         </div>
+     </div> <!-- wrapper -->
+
+         <div class='port-first'>
+           <section class="left-port">
+             <div class='port-text'>
+               <h1> 1. </h1>
+               <h2> PSD to Responsive HTML </h2>
+               <p>HTML5 / CSS3 / Sass / Responsive Design</p>
+               <button class='viewit'><a href="#" target='_blank'>View</a></button>
+             </div>  
+           </section>  
+           <section class='right-port'>
+             <figure><img src="<?php bloginfo('template_url')?>/images/brush.png" alt="brush website image"></figure>
+           </section>   
+          </div> 
+
+         <div class='port-second'> 
+           <section class="left-port">
+           <section class='right-port'>
+              <div class='port-text'>
+                <h1> 2. </h1>
+                <h2> API WEB APPLICATION </h2>
+                <p>Javascript / jQuery / API </p>
+                <button class='viewit'><a href="#" target='_blank'>View</a></button>
+              </div>
+           </section>   
+             <figure><img src="<?php bloginfo('template_url')?>/images/whiskey.png" alt="monserrat website image"></figure>
+           </section>  
+         </div>   
+
+          <div class='port-third'> 
+           <section class="left-port">
+              <div class='port-text'>
+               <h1> 3. </h1>
+               <h2> Client API Project </h2>
+               <p>HTML5 / CSS3 / JavaScript / jQuery / API / Git</p>
+               <button class='viewit'><a href="#" target='_blank'>View</a></button>
+              </div>
+           </section>  
+           <section class='right-port'>
+              <figure><img src="<?php bloginfo('template_url')?>/images/beaus.png" alt="beaus beer website image"></figure>
+           </section>   
+         </div> 
+
+         <div class='port-fourth'> 
+           <section class='right-port'>
+              <div class='port-text'>
+                <h1> 4. </h1>
+                <h2> API JQUERY WEB APP </h2>
+                <p>AJAX / JavaScript / jQuery / Sass / Pair Programming</p>
+                <button class='viewit'><a href="http://www.shialabeoufquiz.com/" target='_blank'>View</a></button>
+                </div>
+           </section>   
+           <section class="left-port">
+             <figure><img src="<?php bloginfo('template_url')?>/images/shia.png" alt="lcbo api website image"></figure>
+           </section>  
+         </div> 
+   </div>
+</div>
 
   <!--  CONTACT -->
   <div id="sectionfour"></div> 
